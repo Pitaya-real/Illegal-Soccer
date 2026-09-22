6185ece010982bc9cd9bf956d25ea8f04793e6c9
Pitaya-real <pitayauisupport@gmail.com>
2026-09-22 16:26:29 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@
 -- =================================================================
 
 -- 1. LOAD THƯ VIỆN PITAYA UI
-local PitayaUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pitaya-real/PitayaUI/refs/heads/main/Pitayauisource.lua?v=" .. tick()))()
+local PitayaUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pitaya-real/PitayaUI/refs/heads/main/Pitayauisource.lua"))()
 
 -- 2. KHỞI TẠO CÁC SERVICE ROBLOX
 local Players = game:GetService("Players")
